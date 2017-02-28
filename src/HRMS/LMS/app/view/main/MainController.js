369c4d24515d57cba4a19ce47b3e74524ba834eb
shivashankarr1 <shivashankar@celestialsys.com>
2017-02-28 15:22:49 +0530

    change of folderstructure
    Signed-off-by: shivashankarr1 <shivashankar@celestialsys.com>

---
+++ src/HRMS/LMS/app/view/main/MainController.js
@@ -0,0 +1,24 @@
+/**
+ * This class is the controller for the main view for the application. It is specified as
+ * the "controller" of the Main view class.
+ *
+ * TODO - Replace this content of this view to suite the needs of your application.
+ */
+Ext.define('LMS.view.main.MainController', {
+     extend: 'Ext.app.ViewController',
+    alias: 'controller.pie-basic',
+
+    onPreview: function () {
+        var chart = this.lookupReference('chart');
+        chart.preview();
+    },
+
+    onDataRender: function (v) {
+        return v + '%';
+    },
+
+    onSeriesTooltipRender: function (tooltip, record, item) {
+        tooltip.setHtml(record.get('os') + ': ' + record.get('data1') + '%');
+    }
+
+});
