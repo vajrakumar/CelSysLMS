82071b80270d9a02f3f2dccd21cdf2d282af4e4f
Vajrakumar <vajrakumar.d@gmail.com>
2017-03-09 15:29:33 +0530

    Cleanup repo

--- src/HRMS/build/temp/development/LMS/sass/config.rb
+++ src/HRMS/build/temp/development/LMS/sass/config.rb
@@ -1,2 +1,2 @@
 Compass.add_project_configuration('../../../../../LMS/sass/config.rb')
-cache_path = '/home/shiva/Desktop/celestialhrms/src/HRMS/build/.sass-cache'
+cache_path = '/Users/vajra/Celestial_Projects/CelSysLMS/src/HRMS/build/.sass-cache'
--- src/HRMS/build/temp/development/LMS/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /home/shiva/Desktop/celestialhrms/src/HRMS/LMS/app.js
